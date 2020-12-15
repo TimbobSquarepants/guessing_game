b3d6aee35e3679d1d439ebb691a752bfe67fe62b
timbobsqaurepants <timothydavidfinn1@gmail.com>
2020-12-15 21:32:32 +1030

    Finished Chapter 2 in the rust book.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,40 @@
-
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
-
 
 fn main() {
     println!("Guess the Number!");
-    println!("Please Input your guess");
-    
 
-    let mut guess = String::new();
+    let secret_number = rand::thread_rng().gen_range(1, 100);
 
-    
 
-    io::stdin()
-    .read_line(&mut guess)
-    .expect("Failed to read line");
+    loop {
+        println!("Please Input your guess");
 
-    println!("You guessed: {}", guess);
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim()
+            .parse(){
+                Ok(num) => num,
+                Err(_) => {
+                    println!("Got a invalid Character");
+
+                    continue;
+                }
+            };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small"),
+            Ordering::Greater => println!("Greater"),
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            }
+        }
+
+        println!("You guessed: {}", guess);
+    }
 }
